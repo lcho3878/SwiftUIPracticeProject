955836726f7aaf3699e79ef6a8bf20e2094d930c
lcho3878 <leech3878@naver.com>
2024-09-02 14:09:42 +0900

    Initial Commit

---
+++ SwiftUIPracticeProject/SwiftUIPracticeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeProjectApp.swift
+//  SwiftUIPracticeProject
+//
+//  Created by 이찬호 on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
